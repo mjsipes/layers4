6ed9e4a63da1e087d359e5161059f7b1ef8f5fd2
Michael Sipes <msipes@usc.edu>
2025-07-02 15:06:07 -0700

    get rid of images

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
