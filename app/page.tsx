c18aa3b296777351dc4e3420d1fcbc2735f3ad9d
Michael Sipes <msipes@usc.edu>
2025-07-02 15:26:39 -0700

    delete footer

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,11 @@ export default function Home() {
               <h1 className="text-4xl font-extrabold tracking-tight text-primary">
                 Layers
               </h1>
-
             </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+            <div className="flex flex-row">
+              <ThemeSwitcher />
+              {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+            </div>
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
@@ -29,21 +31,6 @@ export default function Home() {
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
           </main>
         </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
       </div>
     </main>
   );
--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -18,27 +18,13 @@ export default function ProtectedLayout({
             <h1 className="text-4xl font-extrabold tracking-tight text-primary">
               Layers
             </h1>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+            <div className="flex flex-row">
+              <ThemeSwitcher />
+              {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+            </div>
           </div>
         </nav>
-        <div className="flex-1 w-full">
-          {children}
-        </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
+        <div className="flex-1 w-full">{children}</div>
       </div>
     </main>
   );
