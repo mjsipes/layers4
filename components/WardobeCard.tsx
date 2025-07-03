22adcf6189216de121239eba178e90f13aeb4fa0
mjsipes <msipes@usc.edu>
2025-07-03 13:43:54 -0700

    added layers table

--- components/WardobeCard.tsx
+++ components/WardobeCard.tsx
@@ -2,6 +2,14 @@
 "use client";
 import React, { useEffect } from 'react'
 import { useLayerStore } from '@/stores/store'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 const WardobeCard = () => {
   const { layers, fetchLayers, subscribeToLayers } = useLayerStore();
@@ -18,15 +26,33 @@ const WardobeCard = () => {
       {layers.length === 0 ? (
         <p>No layers found</p>
       ) : (
-        <ul>
-          {layers.map((layer) => (
-            <li key={layer.id}>
-              <h3>{layer.name || 'Unnamed Layer'}</h3>
-              {layer.description && <p>{layer.description}</p>}
-              {layer.warmth && <p>Warmth: {layer.warmth}</p>}
-            </li>
-          ))}
-        </ul>
+        <Table className="table-fixed w-full">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-6/20">Name</TableHead>
+              <TableHead className="w-1/2">Description</TableHead>
+              <TableHead className="w-2/20 text-center">Warmth</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {layers.map((layer) => (
+              <TableRow
+                key={layer.id}
+                className="hover:bg-muted/50 cursor-pointer"
+              >
+                <TableCell className="font-medium truncate">
+                  {layer.name || 'Unnamed Layer'}
+                </TableCell>
+                <TableCell className="font-medium truncate">
+                  {layer.description || '-'}
+                </TableCell>
+                <TableCell className="font-medium truncate text-center">
+                  {layer.warmth || '-'}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       )}
     </div>
   )
