87c88969d48a10ace09e3d558efc31e1e49e9530
mjsipes <msipes@usc.edu>
2025-07-29 15:36:20 -0700

    1

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,10 +10,6 @@ export default function ProtectedLayout({
     <main className="h-screen w-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col items-center">
         <Navbar showDashboardButton={false} />
-        {/* <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-          strategy="beforeInteractive"
-        /> */}
         <div className="flex-1 w-full">{children}</div>
       </div>
     </main>
