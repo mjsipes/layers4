f3fbe7f8de81c474f63be8e2c90562bcf3811839
mjsipes <msipes@usc.edu>
2025-08-27 02:25:50 -0700

    w

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar({ showDashboardButton = false }: NavbarProps) {
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-14 bg-background/50 backdrop-blur-sm pointer-events-auto">
       <div className="w-full flex justify-between items-center p-3 px-3 text-sm">
         <div className="flex gap-2 items-center font-semibold">
-          <h1 className="text-2xl font-extrabold tracking-tight text-primary">
+          <h1 className="text-3xl font-extrabold tracking-tight text-primary">
             Layers
           </h1>
         </div>
